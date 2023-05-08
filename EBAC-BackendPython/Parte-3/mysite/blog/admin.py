fb335262f9d0e05603787b4ee82943ae933a6a45
franc <franckallyson@hotmail.com>
2023-05-08 11:02:17 -0300

    criação admin models e registros

--- EBAC-BackendPython/Parte-3/mysite/blog/admin.py
+++ EBAC-BackendPython/Parte-3/mysite/blog/admin.py
@@ -2,3 +2,22 @@ from django.contrib import admin
 from .models import Post, Author, Category
 # Register your models here.
 
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "slug", "status", "created_on")
+    list_filter = ('status',)
+    search_fields = ('title', 'content')
+    prepopulated_fields = {'slug': ('title',)}
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('user', 'bio', 'photo')
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    search_fields = ('name',)
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Category, CategoryAdmin)
