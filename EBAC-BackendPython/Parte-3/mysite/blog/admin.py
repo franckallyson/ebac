57756400e88bf6010a5ddb599f63620c617e6718
franc <franckallyson@hotmail.com>
2023-05-09 09:35:34 -0300

    Adição Model e ModelAdmin dos Comments

--- EBAC-BackendPython/Parte-3/mysite/blog/admin.py
+++ EBAC-BackendPython/Parte-3/mysite/blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post, Author, Category
+from .models import Post, Author, Category, Comment
 # Register your models here.
 
 class PostAdmin(admin.ModelAdmin):
@@ -16,6 +16,16 @@ class AuthorAdmin(admin.ModelAdmin):
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'description')
     search_fields = ('name',)
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("name", "body", "post", "created_on", "active")
+    list_filter = ("active", "created_on")
+    search_fields = ("name", "email", "body")
+    actions = ("approve_comments")
+
+    def approve_comments(self, request, queryset):
+        queryset.update(active=True)
 
 
 admin.site.register(Post, PostAdmin)
