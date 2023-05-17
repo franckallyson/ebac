1dea0645cfdc5234a0137355f81533eb28cc7504
franc <franckallyson@hotmail.com>
2023-05-17 09:49:09 -0300

    Adição debug_toolbar

--- EBAC-BackendPython/Parte-3/bookstore/bookstore/settings.py
+++ EBAC-BackendPython/Parte-3/bookstore/bookstore/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_extensions',
     'rest_framework',
+    'debug_toolbar',
     'order',
     'product',
 ]
@@ -51,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'bookstore.urls'
@@ -122,3 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
