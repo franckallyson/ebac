cf0caedf3f23c81e407c42e7268efcd5c53c35fe
franc <franckallyson@hotmail.com>
2023-05-18 10:09:35 -0300

    Configuração token authentication

--- EBAC-BackendPython/Parte-3/bookstore/bookstore/settings.py
+++ EBAC-BackendPython/Parte-3/bookstore/bookstore/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_extensions',
     'rest_framework',
+    'rest_framework.authtoken',
     'debug_toolbar',
     'order',
     'product',
@@ -135,5 +136,6 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
     ],
 }
