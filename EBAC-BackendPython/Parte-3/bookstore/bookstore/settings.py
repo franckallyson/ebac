e60d073384a64726f698abbf443f79f817f77e3f
franc <franckallyson@hotmail.com>
2023-05-23 10:04:56 -0300

    Modificação nas variáveis DEBUG e SECRET_KEY

--- EBAC-BackendPython/Parte-3/bookstore/bookstore/settings.py
+++ EBAC-BackendPython/Parte-3/bookstore/bookstore/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'k&8$uwj44@q0psk!y51kg#cxkn&0!poc=029iu%fzfl9im^nlc'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG")
 
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
