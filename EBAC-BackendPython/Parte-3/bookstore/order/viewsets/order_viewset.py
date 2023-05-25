6f493435bd399c0dbe8a61e37cfc8600e35e1be0
franc <franckallyson@hotmail.com>
2023-05-25 10:23:54 -0300

    Retirando autenticações

--- EBAC-BackendPython/Parte-3/bookstore/order/viewsets/order_viewset.py
+++ EBAC-BackendPython/Parte-3/bookstore/order/viewsets/order_viewset.py
@@ -1,12 +1,10 @@
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.permissions import IsAuthenticated
 
 from order.models import Order
 from order.serializers import OrderSerializer
 
 
 class OrderViewSet(ModelViewSet):
-    permission_classes = [IsAuthenticated]
 
     serializer_class = OrderSerializer
     queryset = Order.objects.all().order_by('id')
